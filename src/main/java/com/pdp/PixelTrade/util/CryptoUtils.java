096ff06361352372d3f197a4db4a4c0d8bd30af3
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-10-01 13:34:03 +0500

    added exceptions in CryptoUtils if we have errors on runtime

--- src/main/java/com/pdp/PixelTrade/util/CryptoUtils.java
+++ src/main/java/com/pdp/PixelTrade/util/CryptoUtils.java
@@ -2,6 +2,9 @@ package com.pdp.PixelTrade.util;
 
 import com.pdp.PixelTrade.dto.util.DecryptedDataDTO;
 import com.pdp.PixelTrade.enums.CryptoType;
+import com.pdp.PixelTrade.handler.exceptions.crypto.DecryptionException;
+import com.pdp.PixelTrade.handler.exceptions.crypto.EncryptionException;
+import com.pdp.PixelTrade.handler.exceptions.validation.InvalidDataException;
 import lombok.experimental.UtilityClass;
 
 import javax.crypto.Cipher;
@@ -34,7 +37,7 @@ public class CryptoUtils {
             byte[] encryptedBytes = cipher.doFinal(input.getBytes());
             return new BigInteger(1, encryptedBytes).toString(36);
         } catch (Exception e) {
-            throw new RuntimeException("Error occurred while encrypting: " + e.getMessage(), e);
+            throw new EncryptionException("Error occurred while encrypting: {0}", input);
         }
     }
 
@@ -43,19 +46,17 @@ public class CryptoUtils {
             SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
             Cipher cipher = cipherThreadLocal.get();
             cipher.init(Cipher.DECRYPT_MODE, key);
-
             byte[] decryptedBytes = new BigInteger(encrypted, 36).toByteArray();
             return new String(cipher.doFinal(decryptedBytes));
         } catch (Exception e) {
-            throw new RuntimeException("Error occurred while decrypting: " + e.getMessage(), e);
+            throw new DecryptionException("Error occurred while decrypting: {0}", encrypted);
         }
     }
 
     public DecryptedDataDTO decryptData(String data) {
         String[] split = data.split("\\.");
-        if (split.length != 2) {
-            throw new IllegalArgumentException("Invalid format: expected 'walletID.encryptedCryptoType'");
-        }
+        if (split.length != 2)
+            throw new InvalidDataException("Invalid format: expected 'walletID.encryptedCryptoType'");
         String walletID = split[0];
         CryptoType cryptoType = CryptoType.valueOf(decrypt(split[1]));
         return new DecryptedDataDTO(walletID, cryptoType);
