61bc091a797405c688ea9a5e1c8bfb4c96aab8cd
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-10-01 17:15:48 +0500

    new method for getting authorities easily

--- src/main/java/com/pdp/PixelTrade/enums/Role.java
+++ src/main/java/com/pdp/PixelTrade/enums/Role.java
@@ -2,8 +2,11 @@ package com.pdp.PixelTrade.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.pdp.PixelTrade.enums.Permission.*;
 
@@ -45,5 +48,14 @@ public enum Role {
     ));
 
     private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getAccess()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
 }
 
