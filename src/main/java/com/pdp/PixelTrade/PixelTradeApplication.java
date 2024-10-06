cace20d53bcb6b94a6ddd28162acbbace89c0bcc
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-10-06 16:28:37 +0500

    enabled caching , scheduling

--- src/main/java/com/pdp/PixelTrade/PixelTradeApplication.java
+++ src/main/java/com/pdp/PixelTrade/PixelTradeApplication.java
@@ -4,15 +4,21 @@ import com.pdp.PixelTrade.config.security.SessionUser;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 
 @EnableAsync
 @EnableJpaAuditing
+@EnableCaching
+@EnableScheduling
+@EnableJpaRepositories
 @SpringBootApplication
 public class PixelTradeApplication {
 
