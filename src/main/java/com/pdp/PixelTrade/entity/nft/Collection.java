cfafcc16e46946e13692db08f1cd8f3fc180aedc
Donyor-Nishonov <nishonovd80@gmail.com>
2024-10-10 10:23:57 +0500

    Collection statistics are released in a separate class

--- src/main/java/com/pdp/PixelTrade/entity/nft/Collection.java
+++ src/main/java/com/pdp/PixelTrade/entity/nft/Collection.java
@@ -6,7 +6,6 @@ import com.pdp.PixelTrade.enums.CryptoType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
