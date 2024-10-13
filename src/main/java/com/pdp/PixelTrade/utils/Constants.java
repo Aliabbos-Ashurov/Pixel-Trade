a4e93dbcddfcebafa8f9c9ec5b9054540aa08fcd
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-10-13 15:50:05 +0500

    new interface for Constants

---
+++ src/main/java/com/pdp/PixelTrade/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.pdp.PixelTrade.utils;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 13/October/2024  15:06
+ **/
+public interface Constants {
+    String BASE_PATH_V1 = "/api/v1";
+    String AUTH_TYPE = "Bearer ";
+    String AUTH_HEADER = "Authorization";
+    String[] OPEN_PAGES = {
+            "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+            "/api/v1/auth/**", "/api/v1/**"
+
+    };
+}
