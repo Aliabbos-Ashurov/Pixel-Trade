98510c8354703d3f9345e649afb349bcdb7af97c
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-12-14 16:34:41 +0500

    springdoc has written

--- src/main/java/com/pdp/PixelTrade/controller/rest/AuthRestController.java
+++ src/main/java/com/pdp/PixelTrade/controller/rest/AuthRestController.java
@@ -5,9 +5,11 @@ import com.pdp.PixelTrade.dto.auth.*;
 import com.pdp.PixelTrade.service.UserService;
 import com.pdp.PixelTrade.service.token.TokenService;
 import com.pdp.PixelTrade.utils.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,23 +28,37 @@ public class AuthRestController {
     private final TokenService tokenService;
     private final UserService userService;
 
-    @PostMapping(value = "/token",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(method = "POST",
+            description = "to get token through username & password",
+            responses = @ApiResponse(
+                    responseCode = "200/201",
+                    description = "success",
+                    content = @Content(mediaType = "application/json")))
+    @PostMapping(value = "/token")
     public ResponseEntity<Response<TokenResponseDTO>> login(@Valid @RequestBody TokenRequestDTO dto) {
         return ResponseEntity.ok(tokenService.generateToken(dto.username(), dto.password()));
     }
 
-    @PostMapping(value = "/refresh-token",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            method = "POST",
+            description = "user can refresh token through refresh tokens",
+            responses = @ApiResponse(
+                    responseCode = "401",
+                    description = "if jwt expired",
+                    content = @Content(mediaType = "application/json")))
+    @PostMapping(value = "/refresh-token")
     public ResponseEntity<Response<TokenResponseDTO>> refreshToken(@Valid @RequestBody RefreshTokenRequestDTO dto) {
         return ResponseEntity.ok(tokenService.refreshToken(dto.refreshToken()));
     }
 
-    @PostMapping(value = "/register",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            method = "POST",
+            description = "the way of getting first token",
+            responses = @ApiResponse(
+                    responseCode = "404",
+                    description = "if user has not registered from otp yet"
+            ))
+    @PostMapping(value = "/register")
     private ResponseEntity<Response<UserResponseDTO>> register(@Valid @RequestBody UserCreateDTO dto) {
         return ResponseEntity.ok(userService.create(dto));
     }
