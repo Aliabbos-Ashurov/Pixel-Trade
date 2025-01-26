1497ead387432c0e36dc81f9cf9601143b8d97d2
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-01-26 16:25:15 +0500

    changes

--- src/main/java/com/pdp/PixelTrade/utils/HttpMethod.java
+++ src/main/java/com/pdp/PixelTrade/utils/HttpMethod.java
@@ -1,7 +1,17 @@
 package com.pdp.PixelTrade.utils;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
-* @author Aliabbos Ashurov
-* @since 14/December/2024  16:44
-**/public class HttpMethod {
+ * @author Aliabbos Ashurov
+ * @since 14/December/2024  16:44
+ **/
+@Getter
+@RequiredArgsConstructor
+public class HttpMethod {
+    public static final String _GET = "GET";
+    public static final String _POST = "POST";
+    public static final String _PUT = "PUT";
+    public static final String _DELETE = "DELETE";
 }
